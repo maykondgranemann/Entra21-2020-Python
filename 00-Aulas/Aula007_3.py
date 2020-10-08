4d78ceee381e2a0ed033662986ec6d7f03ac7c3c
Maykon Granemann <maykondgranemann@gmail.com>
2020-10-08 15:14:19 -0300

    Funcoes - Exemplo funcao soma

---
+++ 00-Aulas/Aula007_3.py
@@ -0,0 +1,11 @@
+#=== Funções
+#--- Criação de função para soma de dois numeros inteiros
+
+def soma():
+    numero1 = int(input('digite o numero1: '))
+    numero2 = int(input('digite o numero2: '))    
+    resultado = numero1 + numero2
+    print(f'a soma dos numeros {numero1} e {numero2} é :{soma}')
+
+
+soma()
